a8d5fef2074e9ac32929c604b247885c312952be
Edward Vidal Jr <develone@sbcglobal.net>
2022-11-29 07:48:10 -0700

    required update

--- pico_w/mqtt/mqtt_example.c
+++ pico_w/mqtt/mqtt_example.c
@@ -178,7 +178,7 @@ int main() {
     }
     cyw43_arch_enable_sta_mode();
     printf("Connecting to WiFi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("ATTtpHTfPi", WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
         return 1;
     } else {
